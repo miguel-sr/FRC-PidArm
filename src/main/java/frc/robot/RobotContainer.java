c00cb14ace3dc45ee4649dbcb23aff48335016bf
Miguel S. Ramos <miguelsramos458@gmail.com>
2023-04-02 15:05:53 -0300

    :sparkles: feat: Add Bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,46 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.ArmSubsystem;
 
 public class RobotContainer {
- 
+  
+  private final ArmSubsystem m_robotArm = new ArmSubsystem();
+
+  Joystick m_armController = new Joystick(OIConstants.kArmControllerPort);
+
   public RobotContainer() {
     configureBindings();
   }
   
-  private void configureBindings() {}
+  private void configureBindings() {
+    new JoystickButton(m_armController, OIConstants.kAButton)
+      .onTrue(Commands.runOnce(
+        () -> {
+          m_robotArm.setGoal(2);
+          m_robotArm.enable();
+        }, 
+        m_robotArm));
+
+    // Move the arm to neutral position when the 'B' button is pressed.
+    new JoystickButton(m_armController, OIConstants.kBButton)
+      .onTrue(Commands.runOnce(
+        () -> {
+          m_robotArm.setGoal(ArmConstants.kArmOffsetRads);
+          m_robotArm.enable();
+        }, 
+        m_robotArm));
+  }
+
+  public void disablePIDSubsystems() {
+    m_robotArm.disable();
+  }
   
   public Command getAutonomousCommand() {
     return null;
